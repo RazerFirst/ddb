e966907aa15e1de2641b3d0bb580807ff02d5886
SIah Nail <SIah Nail>
2017-01-22 12:19:52 +0200

    Add settings init

--- querrys/services_get.rb
+++ querrys/services_get.rb
@@ -4,32 +4,52 @@ require 'json'
 
 client = Aws::DynamoDB::Client.new(endpoint: 'http://localhost:8000', region: "us-east-1", access_key_id: 'access_key_id', secret_access_key: 'secret_access_key')
 
-resp = client.batch_get_item({
-  request_items: {
-    "Services" => {
-      keys: [
-        {
-          "serviceName" => "perf-sparksolrslave",
-        }, 
+# resp = client.batch_get_item({
+#   request_items: {
+#     "Services" => {
+#       keys: [
+#         {
+#           "serviceName" => "perf-sparksolrslave",
+#         }, 
         
-      ], 
+#       ], 
       
-    }, 
-  }, 
-})
+#     }, 
+#   }, 
+# })
 
-puts resp.to_h
+# puts resp.to_h
 
 
-puts JSON.pretty_generate(resp.to_h)
+# puts JSON.pretty_generate(resp.to_h)
+
+# puts "============================================="
+
+# resp = client.get_item({
+#   key: {
+#      "serviceName" => "perf-sparksolrslave",
+#   }, 
+#   table_name: "Services", 
+# })
+
+# puts JSON.pretty_generate(resp.to_h)
 
 puts "============================================="
 
-resp = client.get_item({
-  key: {
-     "serviceName" => "perf-sparksolrslave",
-  }, 
-  table_name: "Services", 
-})
+# resp = client.scan({
+#   expression_attribute_names: {
+#     "AT" => "serviceName", 
+#     "ST" => "serviceData", 
+#   }, 
+#   expression_attribute_values: {
+#     ":a" => {
+#       s: "No One You Know", 
+#     }, 
+#   }, 
+#   filter_expression: "serviceName = :a", 
+#   projection_expression: "#ST, #AT", 
+#   table_name: "Services", 
+# })
+resp = client.scan :table_name => "Services"
 
 puts JSON.pretty_generate(resp.to_h)
--- querrys/services_init.rb
+++ querrys/services_init.rb
@@ -9,18 +9,41 @@ resp = client.batch_write_item({
       {
         put_request: {
           item: {
-            "serviceName" => "perf-sparksolrslave",
-            "serviceData": {"port": "8983",
+
+            "serviceName" => "perf-sparkhybriscluster", 
+            "serviceData": {
               "context": {
-                "src": "http://spark:S0Rvc0AhI15pbzc4UnQw@ecsa0040006b.epam.com:8081/artifactory/SPARK/tools/solr/5.3/solr.tar.gz",
-                "src_chksum": "b1a4ef0a268f44613603db9deb65c69ed8b7dca9d23922da0988e9aa5ce30bd3",
+                "cluster": true,
                 "base_dir": "/opt/spark",
-                "init_name": "solrslave"
+                "user": "hybris",
+                "user_home": "/home/hybris",
+                "accelerator": "b2c",
+                "snapshot": {
+                  "version": "1.1.20-master",
+                  "url": "http://spark:S0Rvc0AhI15pbzc4UnQw@ecsa0040006b.epam.com:8081/artifactory/SPARK/hybris/b2c/snapshot/"
+                },
+                "platform": {
+                  "version": "6200P_1-80002118",
+                  "url": "http://epmd-dspk:APyyj1CjkremeHK@ecsa0040006b.epam.com:8081/artifactory/SPARK_MAINTENANCE/hybris/platform/",
+                  "checksum": "901043e07e48a75c7a6634b08b7856193ba4eb46a64b18ddde78256e35174af4",
+                  "licence": "http://epmd-dspk:APyyj1CjkremeHK@ecsa0040006b.epam.com:8081/artifactory/SPARK_MAINTENANCE/hybris/licence/hybrisServer-Licence.zip"
+                }
               },
               "state": "installed",
               "depends_on": [
-                "perf-sparksolrmaster"
-              ]},
+                "perf-sparkmysql",
+                "perf-sparksolrmaster",
+                "perf-sparksolrslave",
+                "perf-sparklb",
+                "perf-sparknfsserver",
+                "perf-sparknfsclient",
+                "perf-sparkhybrisclustermaster"
+              ]
+            },
+ 
+
+
+
           }, 
         }, 
       }, 
